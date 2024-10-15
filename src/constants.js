ceff2758dc040b20387d5ec2e538be000fd0dcaa
Brian Tan <bharveytan@gmail.com>
2024-10-14 18:31:58 -0700

    constants

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const DEFAULT_BG_COLOR = '#F4F6F8';
+export const DEFAULT_ARROW_COLOR = '#343A40';
+export const SELECTED_BG_COLOR = '#E5E8FD';
+export const SELECTED_ARROW_COLOR = '#253CF2';
