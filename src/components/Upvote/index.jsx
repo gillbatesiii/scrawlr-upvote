782bad24323959566796d7f6ca119849b88f91b0
Brian Tan <bharveytan@gmail.com>
2024-10-14 22:39:21 -0700

    use button instead of div in Upvote component

--- src/components/Upvote/index.jsx
+++ src/components/Upvote/index.jsx
@@ -1,11 +1,12 @@
 import ArrowUp from './arrow-up.svg?react';
+import './upvote_styles.css';
 import {DEFAULT_ARROW_COLOR, DEFAULT_BG_COLOR, SELECTED_BG_COLOR, SELECTED_ARROW_COLOR} from "~/constants.js";
 
 export default function Upvote({selected, onSelectionToggle}) {
     return (
-        <div onClick={() => onSelectionToggle(!selected)}
+        <button className={'upvote'} onClick={() => onSelectionToggle(!selected)}
              style={{backgroundColor: selected ? SELECTED_BG_COLOR : DEFAULT_BG_COLOR}}>
             <ArrowUp fill={selected ? SELECTED_ARROW_COLOR : DEFAULT_ARROW_COLOR}/>
-        </div>
+        </button>
     )
 }
