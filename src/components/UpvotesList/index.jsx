24a7e71692f2479fdf6250ade4e27d58625f0afb
Brian Tan <bharveytan@gmail.com>
2024-10-15 17:00:08 -0700

    less confusing name for upvoteComponentsList

--- src/components/UpvotesList/index.jsx
+++ src/components/UpvotesList/index.jsx
@@ -2,18 +2,18 @@ import Upvote from "~/components/Upvote/index.jsx";
 import Plus from './plus.svg?react';
 import {useState} from "react";
 
-export default function UpvotesList() {
+export default function UpvotesList({id}) {
     const [isSelected, setIsSelected] = useState(false);
     const [upvotesCount, setUpvotesCount] = useState(0);
 
-    let upvoteList = [];
+    let upvoteComponentsList = [];
 
     for (let i = 0; i < upvotesCount; i++) {
-        upvoteList.push(<Upvote key={i} selected={isSelected} onSelectionToggle={setIsSelected} />);
+        upvoteComponentsList.push(<Upvote key={i} selected={isSelected} onSelectionToggle={setIsSelected} />);
     }
 
     return <div>
-        {upvoteList}
+        {upvoteComponentsList}
         <button onClick={() => setUpvotesCount(prevCount => prevCount + 1)}><Plus /></button>
     </div>
 }
